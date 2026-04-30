a3f84e1eb5b69076119d908d87833c517aae604a
Zander Fields <zander28fields@gmail.com>
2026-04-30 14:33:08 -0600

    Fix import statement in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
